c66051ececc09ca946cfce2e05d7f3cd55bfc25c
patron290 <balikapatrik@gmail.com>
2024-03-21 20:40:56 +0100

    add logging

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -1,9 +1,16 @@
-use actix_web::{web, HttpResponse, Responder};
+use actix_multipart::Multipart;
+use actix_web::{Error, HttpResponse, Result};
+use env_logger;
+use futures::TryStreamExt;
+use log::info;
 
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
+#[actix_web::post("/upload_video")]
+async fn upload_video(mut playload: Multipart) -> Result<HttpResponse, Error> {
+    env_logger::init();
+    while let Ok(Some(field)) = playload.try_next().await {
+        let content_type = field.content_disposition();
+        info!("Content-Type: {:?}", content_type);
+    }
 
-pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::resource("/").route(web::get().to(hello)));
+    Ok(HttpResponse::Ok().into())
 }
