ff2a3ff0e25a5fa14225e0b48dd59ef024022095
Balogh Patrik <balikapatrik@gmail.com>
2024-03-23 09:23:29 +0100

    Fix the logger

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -1,16 +1,18 @@
 use actix_multipart::Multipart;
-use actix_web::{Error, HttpResponse, Result};
-use env_logger;
+use actix_web::{Error, HttpResponse, Result, web};
 use futures::TryStreamExt;
 use log::info;
 
-#[actix_web::post("/upload_video")]
-async fn upload_video(mut playload: Multipart) -> Result<HttpResponse, Error> {
+async fn upload_video(mut payload: Multipart) -> Result<HttpResponse, Error> {
     env_logger::init();
-    while let Ok(Some(field)) = playload.try_next().await {
+    while let Ok(Some(field)) = payload.try_next().await {
         let content_type = field.content_disposition();
         info!("Content-Type: {:?}", content_type);
     }
 
-    Ok(HttpResponse::Ok().into())
+    Ok(HttpResponse::Ok().finish())
+}
+
+pub fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(web::resource("/upload_video").route(web::post().to(upload_video)));
 }
