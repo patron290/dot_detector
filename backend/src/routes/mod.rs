1d7f0a2a4cebe655085c9be4c9942906cce3bd9b
Balogh Patrik <balikapatrik@gmail.com>
2024-03-23 19:21:06 +0100

    Finish video upload

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -1,13 +1,23 @@
+use tokio::{fs::File, io::AsyncWriteExt};
 use actix_multipart::Multipart;
 use actix_web::{Error, HttpResponse, Result, web};
-use futures::TryStreamExt;
-use log::info;
+use futures::{StreamExt, TryStreamExt};
 
 async fn upload_video(mut payload: Multipart) -> Result<HttpResponse, Error> {
-    env_logger::init();
-    while let Ok(Some(field)) = payload.try_next().await {
-        let content_type = field.content_disposition();
-        info!("Content-Type: {:?}", content_type);
+    while let Ok(Some(mut field)) = payload.try_next().await {
+        let content_disposition  = field.content_disposition();
+        let filename = match content_disposition.get_filename() {
+            Some(filename) => filename,
+            None => return Err(actix_web::error::ErrorBadRequest("No filename present")),
+        };
+
+        let filepath = format!("./uploads/{}", sanitize_filename::sanitize(&filename));
+        let mut f = File::create(filepath).await?;
+
+        while let Some(chunk) = field.next().await {
+            let data = chunk?;
+            f.write_all(&data).await?;
+        }
     }
 
     Ok(HttpResponse::Ok().finish())
