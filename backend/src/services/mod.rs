6ce5ebd20a498997000ac054ac55f2c1736760bd
Balogh Patrik <balikapatrik@gmail.com>
2024-03-27 21:13:03 +0100

    you should more in your free time

--- backend/src/services/mod.rs
+++ backend/src/services/mod.rs
@@ -8,4 +8,16 @@ async fn save_video_frames_as_png(video_path: &Path) -> Result<(), ffmpeg::Error
 
     let mut ictx = format::input(&video_path)?;
     let input_stram = ictx.streams().best(Type::Video).expect("No video stream found");
+    let video_stream_index = input_stram.index();
+
+    let mut decoder = input_stram.codec().decoder().video()?;
+    let mut scaler = scaling::Context::get(
+        decoder.format(),
+        decoder.width(),
+        decoder.height(),
+        ffmpeg::ffi::AVPixelFormat::AV_PIX_FMT_RGB24,
+        decoder.width(),
+        decoder.height(),
+        Flags::BILINEAR,
+    )?;
 }
